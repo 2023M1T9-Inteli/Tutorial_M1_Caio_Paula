f019efd133038251b82ee768bf8959c6ec6b9833
CaioPaula <caio.paula@sou.inteli.edu.br>
2023-02-02 11:18:38 -0300

    Alteração do código: Velocidade

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var status = 0 #A variável status define se o o jogo está ou não sendo executado. Prefito abrir o jogo e depois executá-lo
+var vscore = 0 #O vscore representa o valor da pontuação, que depois será projetado na tela.
+var x = 3.5 #X é o valor que depois será usado para a movimentação horizontal do jogo, do cenário e colunas no caso-> mudei o valor de x para 3.5, assim o cenário corre mais rápido
+var y = 1 #Y é o valor que será usado para a movimentação vertical no joco, do dração no caso
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -17,12 +17,12 @@ func _process(delta):
 	if status == 1: # jogando
 		
 		# movimenta o cenário do fundo
-		$background.position.x -= 1*x
+		$background.position.x -= 1.5*x #--> aumentei o multiplicador da posição x do cenário, apra acelerá-lo
 		if ($background.position.x) < -200:
 			$background.position.x = 600
 			
 		# movimenta as colunas para colisão
-		$columns.position.x -= 2*x
+		$columns.position.x -= 2*x #--> aumentei das colunas também
 		if ($columns.position.x) < -550:
 			$columns.position.x = rand_range(0, 350) - 50
 			$columns.position.y = rand_range(0, 400) - 200
@@ -41,13 +41,13 @@ func _process(delta):
 			
 		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_down"):
-			$dragon.position.y += 2
+			$dragon.position.y += 3.5 #para descer mais rápido, aumentei a valor da movimentação vertical quando seta-para-baixo é apertada
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 5 #nesse caso, a velocidade é maior para cima
 			
-	elif status == 0: # parado
+	elif status == 0: # parado 
 		
 		$dragon/dragonImages.playing = false # faz dragão parar de bater as asas
 		$gameover.show() # exibe imagem gameover
